40cda8c9b5d42f56acb7ed0eaa998bea6e2ec60b
Yerin <myheaven_b@naver.com>
2023-02-16 15:56:03 +0900

    feat: 방명록 구현 마무리

---
+++ js/coverletter.js
@@ -0,0 +1,15 @@
+window.onload = function(){
+    var btnRegister = document.getElementById('btnRegister');
+    var btnCancel = document.getElementById('btnCancel')
+    var guestName = document.getElementById('guestName');
+    
+    btnRegister.addEventListener('click', function(){
+        var checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked')
+        alert(guestName.value + '님, 저와 ' + checkedBoxes.length + '개의 취향이 같으시네요!');
+    })
+
+    btnCancel.addEventListener('click', function() {
+        document.getElementById("guestName").value = null;
+        document.querySelectorAll('input[type="checkbox"]:checked').values = false;
+    })
+}
