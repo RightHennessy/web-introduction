422878e35dcdeaae5c71d72f4d1bfc19fce8df85
Yerin <myheaven_b@naver.com>
2023-02-16 21:12:49 +0900

    feat: Wls찐막

--- js/coverletter.js
+++ js/coverletter.js
@@ -6,7 +6,10 @@ window.onload = function(){
     btnRegister.addEventListener('click', function(){
         var checkedBoxes = document.querySelectorAll('input[type="checkbox"]:checked')
         alert(guestName.value + '님, 저와 ' + checkedBoxes.length + '개의 취향이 같으시네요!');
-    })
+        document.getElementById("result").innerText += (guestName.value + " ");
+        const space = document.createTextNode("\u00a0"); 
+        document.getElementById("result").appendChild(space)
+    })  
 
     btnCancel.addEventListener('click', function() {
         document.getElementById("guestName").value = null;
